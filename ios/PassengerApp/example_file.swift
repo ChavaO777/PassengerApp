6839d10fccce9caad0c53a818bddabdda42ee640
ChavaO777 <salvador@orozco.in>
2018-04-13 11:37:45 -0500

    Fixed the password text field. It is now a secure input.

---
+++ ios/PassengerApp/example_file.swift
@@ -0,0 +1,9 @@
+//
+//  example_file.swift
+//  PassengerApp
+//
+//  Created by Salvador Orozco Villalever on 4/10/18.
+//  Copyright © 2018 Comonfort. All rights reserved.
+//
+
+import Foundation

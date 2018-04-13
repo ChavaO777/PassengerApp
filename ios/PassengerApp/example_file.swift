6e24c5bd4edc7a77858c0ad264fd29b5821c8b80
ChavaO777 <salvador@orozco.in>
2018-04-13 11:46:54 -0500

    Deleted example file

--- ios/PassengerApp/example_file.swift
+++
@@ -1,9 +0,0 @@
-//
-//  example_file.swift
-//  PassengerApp
-//
-//  Created by Salvador Orozco Villalever on 4/10/18.
-//  Copyright © 2018 Comonfort. All rights reserved.
-//
-
-import Foundation

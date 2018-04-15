169696a459627e545a760d1b604c1217a86a88dd
Comonfort <rafaelcomonfort@MacBook-Air-de-Rafael.local>
2018-04-15 12:08:25 -0500

    no sé que estoy haciendo

---
+++ ios/PassengerApp/PassengerApp/CustomButton.swift
@@ -0,0 +1,61 @@
+//
+//  CustomButton.swift
+//  PassengerApp
+//
+//  Created by Comonfort on 4/15/18.
+//  Copyright © 2018 Comonfort. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+@IBDesignable class CustomButton: UIButton {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        sharedInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        sharedInit()
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat = 15 {
+        didSet {
+            refreshCorners(value: cornerRadius)
+        }
+    }
+    
+    @IBInspectable var backgroundImageColor: UIColor = UIColor.init(red: 0, green: 122/255.0, blue: 255/255.0, alpha: 1) {
+        didSet {
+            refreshColor(color: backgroundImageColor)
+        }
+    }
+    
+    func createImage(color: UIColor) -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: 1, height: 1), true, 0.0)
+        color.setFill()
+        UIRectFill(CGRect(x: 0, y: 0, width: 1, height: 1))
+        let image = UIGraphicsGetImageFromCurrentImageContext()!
+        return image
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        sharedInit()
+    }
+    
+    func sharedInit() {
+        refreshCorners(value: cornerRadius)
+        refreshColors(color: backgroundImageColor)
+    }
+    
+    func refreshCorners(value: CGFloat) {
+        layer.cornerRadius = value
+    }
+    
+    func refreshColor(color: UIColor) {
+        let image = createImage(color: color)
+        setBackgroundImage(image, for: UIControlState.normal)
+        clipsToBounds = true
+    }
+}

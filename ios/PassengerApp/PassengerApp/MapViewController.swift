ea5cab99123311b8016681ae73d9e5cc6bf4e380
ChavaO777 <salvador@orozco.in>
2018-04-15 15:26:30 -0500

    The full-screen map is working. It must, however, be displayed in a smaller view.

--- ios/PassengerApp/PassengerApp/MapViewController.swift
+++ ios/PassengerApp/PassengerApp/MapViewController.swift
@@ -11,10 +11,10 @@ import GoogleMaps
 
 class MapViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,15 +27,15 @@ class MapViewController: UIViewController {
         // Create a GMSCameraPosition that tells the map to display the
         // coordinate -33.86,151.20 at zoom level 6.
         let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
-        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
-        view = mapView
-        
+        let mapViewGoogleMaps = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        view = mapViewGoogleMaps
+
         // Creates a marker in the center of the map.
         let marker = GMSMarker()
         marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
         marker.title = "Sydney"
         marker.snippet = "Australia"
-        marker.map = mapView
+        marker.map = mapViewGoogleMaps
     }
 
     /*

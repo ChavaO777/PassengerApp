40586a67a7350bdc0cabb8293254c339772cd93e
Andrés Reynoso Mazoy <andres.reynoso.mazoy@gmail.com>
2018-04-15 19:02:59 -0500

    Created MainActivity, with the BottomNavigationView and the 5 Fragments.

---
+++ android/app/src/main/java/com/example/andresr/passengerappandroid/activities/AddEditTripFragment.java
@@ -0,0 +1,20 @@
+package com.example.andresr.passengerappandroid.activities;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.andresr.passengerappandroid.R;
+
+public class AddEditTripFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_add_edit_trip, container, false);
+    }
+}
---
+++ android/app/src/main/java/com/example/andresr/passengerappandroid/activities/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.andresr.passengerappandroid.activities;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.andresr.passengerappandroid.R;
+import com.example.andresr.passengerappandroid.adapters.SectionsStatePagerAdapter;
+import com.example.andresr.passengerappandroid.helpers.BottomNavigationViewHelper;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SectionsStatePagerAdapter sectionsStatePagerAdapter;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewPager = findViewById(R.id.container);
+        setupViewPager(viewPager);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavView_Bar);
+        BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
+        Menu menu = bottomNavigationView.getMenu();
+        MenuItem menuItem = menu.getItem(1);
+        menuItem.setChecked(true);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.menu_calendar:
+                        setViewPager(0);
+                        break;
+
+                    case R.id.menu_map:
+                        setViewPager(1);
+                        break;
+
+                    case R.id.menu_profile:
+                        setViewPager(2);
+                        break;
+                }
+                item.setChecked(true);
+                return false;
+            }
+
+        });
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        SectionsStatePagerAdapter adapter = new SectionsStatePagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new CalendarFragment(), "Calendar");
+        adapter.addFragment(new MapFragment(), "Map");
+        adapter.addFragment(new ProfileFragment(), "Profile");
+        adapter.addFragment(new RateFragment(), "Rate");
+        adapter.addFragment(new AddEditTripFragment(), "Add/Edit Trip");
+        viewPager.setAdapter(adapter);
+    }
+
+    public void setViewPager(int fragmentNumber) {
+        viewPager.setCurrentItem(fragmentNumber);
+    }
+
+}

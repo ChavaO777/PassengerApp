5b2c9296129ca2d22c6ee6024ac808c499c87703
ChavaO777 <salvador@orozco.in>
2018-04-18 15:11:33 -0500

    Added the plist file for the variables 'notifications', 'vibration' and 'sound' for the user profile preferences.

--- ios/PassengerApp/PassengerApp/ProfileViewController.swift
+++ ios/PassengerApp/PassengerApp/ProfileViewController.swift
@@ -9,11 +9,60 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
-
+    
+    var notifications = true
+    var vibration = true
+    var sound = true
+    
+    @IBAction func toggleNotifications(_ sender: UISwitch) {
+    }
+    
+    @IBAction func toggleVibrations(_ sender: UISwitch) {
+    }
+    
+    @IBAction func toggleSound(_ sender: UISwitch) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+    
         // Do any additional setup after loading the view.
+        
+        //Get the values from the ConfigVariables plist file
+        getValueFromPlist(keyName: "notifications")
+        getValueFromPlist(keyName: "vibration")
+        getValueFromPlist(keyName: "sound")
+        
+        print(notifications)
+        print(vibration)
+        print(sound)
+    }
+    
+    func getValueFromPlist(keyName: String){
+        
+        //Path to the ConfigVariables plist file
+        let path = Bundle.main.path(forResource: "ConfigVariables", ofType: "plist")
+        //Dictionary with the key-value structures in that file
+        let dict = NSDictionary(contentsOfFile: path!)
+        
+        //Get the value of the key 'keyName'
+        switch keyName {
+        
+            case "notifications":
+                notifications = dict?.value(forKeyPath: keyName) as! Int == 1 ? true : false
+                break
+            
+            case "vibration":
+                vibration = dict?.value(forKeyPath: keyName) as! Int == 1 ? true : false
+                break
+            
+            case "sound":
+                sound = dict?.value(forKeyPath: keyName) as! Int == 1 ? true : false
+                break
+        default:
+            
+            break
+        }
     }
 
     override func didReceiveMemoryWarning() {

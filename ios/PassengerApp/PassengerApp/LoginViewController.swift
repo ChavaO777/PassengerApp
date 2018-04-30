af7dad347385cb6fdc725f0f029b4be78f8ebab0
ChavaO777 <salvador@orozco.in>
2018-04-30 12:34:05 -0500

    Added the validation of the empty user credentials. Watch out for the view controller the login view is linked to.

--- ios/PassengerApp/PassengerApp/LoginViewController.swift
+++ ios/PassengerApp/PassengerApp/LoginViewController.swift
@@ -9,35 +9,35 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
-    @IBOutlet weak var username: UITextField!
-    @IBOutlet weak var password: UITextField!
+    
+    @IBOutlet weak var userId: UITextField!
+    @IBOutlet weak var userPassword: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    @IBAction func login(_ sender: UIButton) {
     
+    @IBAction func login(_ sender: UIButton) {
+        
         //If the username and/or the password are missing
-        if credentialsAreEmpty ()
-        {
+        if credentialsAreEmpty () {
+            
             //If it is, ask the user for an action
             let alert = UIAlertController(title: "Alerta",
-                                          message: "Para autenticarse, debe proporcionar su nombre de usuario y contraseña.",
+                                          message: "Para autenticarse debe proporcionar su nombre de usuario y contraseña.",
                                           preferredStyle: .alert)
-            
+
             // Discard current trip button
             let okAction = UIAlertAction(title: "Ok.", style: .default, handler: { (action) -> Void in
                 // Go back to login
                 self.dismiss(animated: true, completion: nil)
             })
-            
+
             // Add action buttons and present the Alert
             alert.addAction(okAction)
             present(alert, animated: true, completion: nil)
-            
+
             //The authentication failed!
             return
         }
@@ -45,17 +45,15 @@ class LoginViewController: UIViewController {
     
     private func credentialsAreEmpty() -> Bool {
         
-        let usernameText = username.text
-        let passwordText = password.text
-
-        return (usernameText?.isEmpty)! || (passwordText?.isEmpty)!
+        let userIdText = userId.text
+        let userPasswordText = userPassword.text
+        
+        return (userIdText?.isEmpty)! || (userPasswordText?.isEmpty)!
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    
 }
 

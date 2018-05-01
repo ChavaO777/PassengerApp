b5734c302e314d690d8190ed67846f19a1ce8ee1
Unknown <neme.d@live.com>
2018-04-30 20:37:33 -0500

    working map
    mapview was changed to mapfragment

--- android/app/src/main/java/com/example/andresr/passengerappandroid/activities/MapFragment.java
+++ android/app/src/main/java/com/example/andresr/passengerappandroid/activities/MapFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,57 +17,67 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapFragment extends Fragment implements OnMapReadyCallback {
-    private MapView mapView;
+    private SupportMapFragment mapFragment;
     private GoogleMap map;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_map, container, false);
+        mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        if(mapFragment == null) {
+            FragmentManager fm = getFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            mapFragment = SupportMapFragment.newInstance();
+            ft.replace(R.id.map, mapFragment).commit();
 
+        }
+        mapFragment.getMapAsync(this);
+/*
         mapView = (MapView) v.findViewById(R.id.mapview);
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(this); //this is important
-
+/**/
         return v;
 
     }
     @Override
     public void onResume() {
-        mapView.onResume();
+        //mapView.onResume();
         super.onResume();
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        mapView.onDestroy();
+        //mapView.onDestroy();
     }
 
     @Override
     public void onLowMemory() {
         super.onLowMemory();
-        mapView.onLowMemory();
+        //mapView.onLowMemory();
     }
 
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        mapView.onSaveInstanceState(outState);
+        //mapView.onSaveInstanceState(outState);
     }
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
 
         map = googleMap;
-        MapsInitializer.initialize(this.getActivity());
-        googleMap.getUiSettings().setMyLocationButtonEnabled(false);
-        googleMap.getUiSettings().setZoomControlsEnabled(true);
 
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(19.1150491,-98.2442522), 10));
-        mapView.onResume();
+        LatLng wv = new LatLng(19.1150491,-98.2442522);
+        googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+        googleMap.getUiSettings().setZoomControlsEnabled(true);
+        map.addMarker(new MarkerOptions().position(wv).title("VW Puebla"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(wv));
     }
 }

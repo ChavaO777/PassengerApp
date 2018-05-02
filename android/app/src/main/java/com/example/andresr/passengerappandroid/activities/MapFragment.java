03ff854136b3166a0ec8d15461af79d32dc8ee92
Neme <neme.d@live.com>
2018-05-01 19:15:44 -0500

    Markers for the stations

--- android/app/src/main/java/com/example/andresr/passengerappandroid/activities/MapFragment.java
+++ android/app/src/main/java/com/example/andresr/passengerappandroid/activities/MapFragment.java
@@ -11,33 +11,43 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.example.andresr.passengerappandroid.BuildConfig;
 import com.example.andresr.passengerappandroid.R;
-import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.example.andresr.passengerappandroid.helpers.HttpHandler;
+import com.example.andresr.passengerappandroid.models.Station;
+import com.example.andresr.passengerappandroid.models.Trip;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-import javax.net.ssl.HttpsURLConnection;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MapFragment extends Fragment implements OnMapReadyCallback {
     private SupportMapFragment mapFragment;
     private GoogleMap map;
     private static final String TAG = "MapFragment";
 
+    List<Station> stationList;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_map, container, false);
+        new GetStations().execute();
+        stationList = new ArrayList<>();
         mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         if(mapFragment == null) {
             FragmentManager fm = getFragmentManager();
@@ -46,6 +56,7 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
             ft.replace(R.id.map, mapFragment).commit();
         }
         mapFragment.getMapAsync(this);
+
         return v;
 
     }
@@ -78,38 +89,85 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
 
         map = googleMap;
 
-        LatLng wv = new LatLng(19.1150491,-98.2442522);
+        LatLng wv = new LatLng(19.129328,-98.262249);
         googleMap.getUiSettings().setMyLocationButtonEnabled(true);
         //0googleMap.getUiSettings().setZoomControlsEnabled(true);
         map.addMarker(new MarkerOptions().position(wv).title("VW Puebla"));
         map.moveCamera(CameraUpdateFactory.newLatLng(wv));
         //changed zoom level here
-        map.animateCamera( CameraUpdateFactory.zoomTo( 16.0f ) );
+        map.animateCamera( CameraUpdateFactory.zoomTo( 15.5f ) );
 
-        setStationsMarkers();
+        for (Station station : stationList) {
+            Log.e(TAG, "Added marker " + station);
+            LatLng pos = station.getLatLng();
+            String name = station.getName();
+            map.addMarker(new MarkerOptions().position(pos).title(name));
+        }
     }
+    private class GetStations extends AsyncTask<Void, Void, Void> {
 
-    public void setStationsMarkers() {
-        AsyncTask.execute(new Runnable() {
-            @Override
-            public void run() {
+
+        @Override
+
+        protected void onPreExecute() {
+
+            super.onPreExecute();
+
+            Toast.makeText(getActivity(), "Getting data from server", Toast.LENGTH_LONG).show();
+
+        }
+
+
+        @Override
+
+        protected Void doInBackground(Void... voids) {
+
+            HttpHandler sh = new HttpHandler();
+
+            // Making a request to url and getting response
+
+            String url = BuildConfig.API_URL + "/stations";
+
+            String jsonStr = sh.makeServiceCall(url);
+
+
+            Log.e(TAG, "Response from url: " + jsonStr);
+
+            if (jsonStr != null) {
                 try {
-                    URL serverEndPoint = new URL("http://192.168.4.246:8000");
-                    HttpsURLConnection myConnection = (HttpsURLConnection) serverEndPoint.openConnection();
-                    String msg;
-                    if (myConnection.getResponseCode() == 200) {
-                        msg = "conexion exitosa";
+                    //JSONObject jsonObj = new JSONObject(jsonStr);
+                    JSONArray trips = new JSONArray(jsonStr);
+                    for (int i = 0; i < trips.length(); ++i) {
+
+                        JSONObject t = trips.getJSONObject(i);
+                        LatLng latLong = new LatLng(t.getDouble("lat"), t.getDouble("lng"));
+                        String id = t.getString("id");
+                        String name = t.getString("name");
+                        double lat = t.getDouble("lat");
+                        double lng = t.getDouble("lng");
+                        Station station = new Station(id, name, lat, lng);
+                        stationList.add(station);
                     }
-                    else {
-                        msg = "error" + myConnection.getResponseCode();
-                    }
-                    Log.e(TAG, msg);
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
+                }
+                catch (JSONException e) {
+                    Log.e(TAG, "JSON parsing error: " + e.getMessage());
                 }
             }
-        }); 
+            else {
+
+                Log.e(TAG, "Couldn't get json from server.");
+
+            }
+
+            return null;
+
+        }
+
+
+        @Override
+
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+        }
     }
 }
